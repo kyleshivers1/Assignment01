4ed3c7804f4d2ed624141be0802087ab470d3f5d
kyleshivers1 <kyleshivers1@gmail.com>
2023-06-25 18:45:28 -0700

    final commit

---
+++ js/script.js
@@ -0,0 +1,114 @@
+// STEP 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+//STEP 2
+// 425.2           //number literal expression
+// 'Hello there'   //string literal expression
+// false           //Boolean literal expression
+// null            //null literal expression
+
+//STEP 3
+// (x + y) / 6
+// z * 5 + otherNumber
+
+//STEP 4
+// let strFirstName;
+// let strLastName;
+// let strAddress;
+// let strCity;
+// let strState;
+// let numZipCode;
+// let numYourAge;
+// let strReferralSource;
+// let boolWeMayContactYou;
+
+//STEP 5
+// let strFirstName = 'Kyle';
+// const numZipCode = 12345;
+// let numYourAge = prompt(`What is your age?`);
+
+//STEP 6
+// let newVariable = 6 + `hello there`;
+// console.log(newVariable);
+
+//STEP 7
+// let variableTwo = true + `whats up?`;
+// console.log(variableTwo);
+// let variableThree = 17 + false;
+// console.log(variableThree);
+
+// STEP 8
+// Not valid. The apostrophe in "I'm" is breaking the string. To fix, You should use backticks (``) like the below:
+
+// let someString = `Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."`;
+// console.log(someString);
+
+// STEP 9
+// let x = null;
+// console.log(x);             //null
+// let newNumber;
+// console.log(newNumber);     //undefined
+
+// STEP 10
+// console.log(typeof(`hello`));       //string
+// console.log(typeof(654));           //number
+// console.log(typeof(true));          //Boolean
+// console.log(typeof(new Object()));  //object
+// console.log(typeof(myVariable));    //undefined
+
+//STEP 11
+// alert('Hello ' + 'Kyle Shivers' + ', welcome to the JavaScript class!');
+
+//STEP 12
+// let fullName = 'Kyle Shivers';
+// alert('Hello ' + fullName + ', welcome to the JavaScript class!');
+
+//STEP 13
+// let fullName = 'Kyle Shivers';
+// let course = "JavaScript";
+// alert('Hello ' + fullName + ', welcome to the ' + course + ' class!');
+
+//STEP 14
+// let fullName = 'Kyle Shivers';
+// let course = "JavaScript";
+// alert('Hello ' + fullName + '.' + '\nWelcome to the ' + course + ' class!');
+
+//STEP 15
+// let fullName = prompt('Enter your full name.');
+// let course = "JavaScript";
+// alert('Hello ' + fullName + '.' + '\nWelcome to the ' + course + ' class!');
+
+//STEP 16
+// let fullName = prompt('Enter your full name.');
+// let course = prompt('Enter your course name');
+// alert('Hello ' + fullName + '.' + '\nWelcome to the ' + course + ' class!');
+
+//STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
+
+//STEP 18
+// let x = 20;
+// console.log(x += 20);
+
+//STEP 19
+// let x = 20;
+// console.log(x *= 5);
+
+//STEP 20
+// let x = 20 % 3;
+// console.log(x /= 1);
+
+//STEP 21
+// console.log(425 > 350 || 350 > 590);
+
+//STEP 22
+// let x = 15;
+// let y = 33;
+// let z = 33;
+// console.log(z <= y && z <= x);
